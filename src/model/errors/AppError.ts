90c46a58f4e5cfca61a2715000229076f5d3a331
Jyunior <francisniltodossantos@gmail.com>
2026-01-05 10:45:00 -0300

    feat(model): implementa classe AppError para tratamento global de exceções

--- src/model/errors/AppError.ts
+++ src/model/errors/AppError.ts
@@ -1,3 +1,8 @@
+//Contrato de Erro genérico para a aplicaço
+
+//Classe de erro genérica para a aplicação
+//Usada para erros genéricos da aplicação, como erros de validação, erros de banco de dados, etc.
+
 export class AppError extends Error {
   constructor(public message: string, public code?: string) {
     super(message);
@@ -5,6 +10,8 @@ export class AppError extends Error {
   }
 }
 
+//Classe de erro genérica para autenticação
+//Usada para erros de autenticação, como erros de login, erros de registro, etc.
 export class AuthError extends AppError {
   constructor(message: string, code?: string) {
     super(message, code);
